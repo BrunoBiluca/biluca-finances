3022803c776f4978da03f950ccdeabd6e63e6620
BrunoBiluca <b.b.da.costa@gmail.com>
2024-09-25 12:35:24 -0300

    🔥 Remoção dos códigos relacionados ao SonarQube, não iremos utilizar esse validador nesse momento

--- predict/sonar.ps1
+++
@@ -1,9 +0,0 @@
-docker run `
---rm `
--e SONAR_HOST_URL="http://192.168.0.11:9000"  `
--v "C:\bprojs\biluca-finances\predict:/usr/src" `
-sonarsource/sonar-scanner-cli `
-    -D sonar.projectKey="Biluca-Finan-as---Predi-o" `
-    -D sonar.sources=src,tests `
-    -D sonar.host.url=http://192.168.0.11:9000 `
-    -D sonar.token="sqp_67fdfc2944ec8f5e60d74710c92bd1c7fbab5d75"
