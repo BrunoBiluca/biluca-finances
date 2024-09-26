e9564cc7125b708c762e6fd10267211a391810e3
BrunoBiluca <b.b.da.costa@gmail.com>
2024-09-26 11:56:40 -0300

    ✨ Aumento na quantidade de dados recuperados na tela de tabela de prestação de contas

--- app/lib/accountability/bloc/bloc.dart
+++ app/lib/accountability/bloc/bloc.dart
@@ -10,7 +10,7 @@ class AccountabilityBloc extends Bloc<AccountabilityEvent, AccountabilityState> 
   AccountabilityBloc({required this.repo}) : super(const AccountabilityInitial()) {
     on<FetchAccountabilityEntries>((event, emit) async {
       emit(AccountabilityChanged(
-        entries: await repo.getEntries(limit: loadedPages * 10),
+        entries: await repo.getEntries(limit: limit),
         identifications: await repo.getIdentifications(),
       ));
     });
@@ -18,7 +18,7 @@ class AccountabilityBloc extends Bloc<AccountabilityEvent, AccountabilityState> 
     on<LoadMoreAccountabilityEntries>((event, emit) async {
       loadedPages += 1;
       emit(AccountabilityChanged(
-        entries: await repo.getEntries(limit: loadedPages * 10),
+        entries: await repo.getEntries(limit: limit),
         identifications: await repo.getIdentifications(),
       ));
     });
@@ -26,7 +26,7 @@ class AccountabilityBloc extends Bloc<AccountabilityEvent, AccountabilityState> 
     on<AddAccountabilityEntry>((event, emit) async {
       await repo.add(event.request);
       emit(AccountabilityChanged(
-        entries: await repo.getEntries(limit: loadedPages * 10),
+        entries: await repo.getEntries(limit: limit),
         identifications: await repo.getIdentifications(),
       ));
     });
@@ -34,7 +34,7 @@ class AccountabilityBloc extends Bloc<AccountabilityEvent, AccountabilityState> 
     on<DeleteAccountabilityEntry>((event, emit) async {
       await repo.delete(event.entry);
       emit(AccountabilityChanged(
-        entries: await repo.getEntries(limit: loadedPages * 10),
+        entries: await repo.getEntries(limit: limit),
         identifications: await repo.getIdentifications(),
       ));
     });
@@ -42,7 +42,7 @@ class AccountabilityBloc extends Bloc<AccountabilityEvent, AccountabilityState> 
     on<UpdateAccountabilityEntry>((event, emit) async {
       await repo.update(event.updatedEntry);
       emit(AccountabilityChanged(
-        entries: await repo.getEntries(limit: loadedPages * 10),
+        entries: await repo.getEntries(limit: limit),
         identifications: await repo.getIdentifications(),
       ));
     });
@@ -50,7 +50,7 @@ class AccountabilityBloc extends Bloc<AccountabilityEvent, AccountabilityState> 
     on<UpdateAccountabilityIdentification>((event, emit) async {
       await repo.updateIdentification(event.updatedIdentification);
       emit(AccountabilityChanged(
-        entries: await repo.getEntries(limit: loadedPages * 10),
+        entries: await repo.getEntries(limit: limit),
         identifications: await repo.getIdentifications(),
       ));
     });
@@ -58,9 +58,11 @@ class AccountabilityBloc extends Bloc<AccountabilityEvent, AccountabilityState> 
     on<DeleteAccountabilityIdentification>((event, emit) async {
       await repo.deleteIdentification(event.identificationId);
       emit(AccountabilityChanged(
-        entries: await repo.getEntries(limit: loadedPages * 10),
+        entries: await repo.getEntries(limit: limit),
         identifications: await repo.getIdentifications(),
       ));
     });
   }
+
+  int get limit => loadedPages * 100;
 }
