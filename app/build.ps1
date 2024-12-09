8f6749effdd4b02f3202eda01093a5ea61edd342
BrunoBiluca <b.b.da.costa@gmail.com>
2024-12-09 16:57:24 -0300

    🔧 Novo processo de empacotamento com Inno Setup

--- app/build.ps1
+++ app/build.ps1
@@ -1,24 +1,28 @@
+# ####
+# Verificação de dependências para o processo de empacotamento
+# ####
+
+if (Get-Command iscc -ErrorAction SilentlyContinue) {
+    Write-Host "Inno Setup está instalado."
+}
+else {
+    Write-Error "[ERRO] Inno Setup não está instalado. Instale-o e rode o script novamente."
+    Exit 1
+}
+
+# ####
+# Criação do pacote windows
+# ####
+
 flutter build windows
 
-$publish_path = "./build\windows\x64\runner\Release"
+# ####
+# Compilação do Inno Setup
+# ####
+
 $output_path = "./dist"
-$package_name = "biluca_financas.zip"
-$package_path = "$publish_path\$package_name"
-
-Copy-Item ./windows/sqlite3.dll $publish_path
-
-if (Test-Path $package_path) {
-    Remove-Item $package_path
-}
-
-Compress-Archive -Path "$publish_path/*" -DestinationPath $package_path
-
-while (!(Test-Path $package_path)) {
-    Start-Sleep -s 1
-}
-
 if (-Not (Test-Path $output_path)) {
     New-Item -Name $output_path -ItemType Directory
 }
 
-Move-item $package_path -Destination $output_path -Force
+iscc ./setup.iss
