f27df296a147ca6da925127df493b9721a2e5799
Bruno Costa <b.b.da.costa@gmail.com>
2026-06-02 15:58:54 -0300

    ♻️ Modularização das seções de contabilidade por identificação no relatório mensal

---
+++ app/lib/reports/monthly_report_v2/sections/accountability_by_identifications_section/expenses_per_indentification.dart
@@ -0,0 +1,33 @@
+import 'package:biluca_financas/reports/components/future_handler.dart';
+import 'package:biluca_financas/reports/monthly_report_v2/sections/accountability_by_identifications_section/identifications_view_section.dart';
+import 'package:biluca_financas/reports/monthly_report_v2/services/current_month_report.service.dart';
+import 'package:biluca_financas/reports/monthly_report_v2/services/identification_rreport_info.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesPerIndentification extends StatelessWidget {
+  const ExpensesPerIndentification({
+    super.key,
+    required CurrentMonthReportService service,
+  }) : _service = service;
+
+  final CurrentMonthReportService _service;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Despesas por identificação",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        const SizedBox(height: 20),
+        futureHandler(
+          _service.expensesByIdentification(),
+          (data) => IdentificationsViewSection(
+            data as List<IdentificationReportInfo>,
+          ),
+        ),
+      ],
+    );
+  }
+}
