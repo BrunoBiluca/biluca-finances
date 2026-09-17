2275526471dd207d1ab59ce2d712626d7f471116
BrunoBiluca <b.b.da.costa@gmail.com>
2026-09-17 17:45:21 -0300

    🚧 Hover para os cartões do relatório anual

--- app/lib/common/extensions/color_extensions.dart
+++ app/lib/common/extensions/color_extensions.dart
@@ -4,4 +4,11 @@ extension ColorExtensions on Color {
   Color adaptByLuminance() {
     return computeLuminance() > 0.5 ? Colors.black : Colors.white;
   }
+
+  Color addBrightness(double amount) => Color.fromARGB(
+        (a * 255.0).round().clamp(0, 255),
+        ((r * 255).round() + amount.round()).clamp(0, 255),
+        ((g * 255).round() + amount.round()).clamp(0, 255),
+        ((b * 255).round() + amount.round()).clamp(0, 255),
+      );
 }
--- app/lib/common/ui/reports/summary_value_card/summary_value_card.dart
+++ app/lib/common/ui/reports/summary_value_card/summary_value_card.dart
@@ -1,8 +1,9 @@
+import 'package:biluca_financas/common/extensions/color_extensions.dart';
 import 'package:biluca_financas/common/formatters/formatter.dart';
 import 'package:biluca_financas/common/ui/reports/summary_value_card/summary_card_label.dart';
 import 'package:flutter/material.dart';
 
-class SummaryValueCard extends StatelessWidget {
+class SummaryValueCard extends StatefulWidget {
   final String title;
   final double value;
   final Color color;
@@ -19,40 +20,53 @@ class SummaryValueCard extends StatelessWidget {
   });
 
   @override
+  State<SummaryValueCard> createState() => _SummaryValueCardState();
+}
+
+class _SummaryValueCardState extends State<SummaryValueCard> {
+  bool isHover = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          spacing: 10,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title.toUpperCase(),
-                      key: const Key("título"),
-                      style: Theme.of(context).textTheme.bodySmall?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      Formatter.value(value),
-                      key: const Key("valor"),
-                      style: Theme.of(context).textTheme.displayLarge?.copyWith(color: color),
-                    ),
-                  ],
-                ),
-                label ?? Container(),
-              ],
-            ),
-            Spacer(),
-            subInfo ?? Container(),
-          ],
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (event) => setState(() => isHover = true),
+      onExit: (event) => setState(() => isHover = false),
+      child: Card(
+        color: isHover ? Theme.of(context).cardColor.addBrightness(10) : Theme.of(context).cardColor,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            spacing: 10,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.title.toUpperCase(),
+                        key: const Key("título"),
+                        style: Theme.of(context).textTheme.bodySmall?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        Formatter.value(widget.value),
+                        key: const Key("valor"),
+                        style: Theme.of(context).textTheme.displayLarge?.copyWith(color: widget.color),
+                      ),
+                    ],
+                  ),
+                  widget.label ?? Container(),
+                ],
+              ),
+              Spacer(),
+              widget.subInfo ?? Container(),
+            ],
+          ),
         ),
       ),
     );
